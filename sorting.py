57f7dd27e90fb517332b109070b4be56c90554c2
joost <joost.baks@gmail.com>
2020-10-27 17:17:48 +0100

    PyMongo

---
+++ sorting.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from pymongo import MongoClient, DESCENDING
+
+client = MongoClient('mongodb://localhost:27017/')
+
+with client:
+
+    db = client.testdb
+
+    cars = db.cars.find().sort("price", -1)
+
+    for car in cars:
+        print('{0} {1}'.format(car['name'], 
+            car['price']))
